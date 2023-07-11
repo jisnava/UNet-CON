be646816809474a0e4dff96d761a962b232aea7a
jisnava <jisnava@gmail.com>
2023-07-11 16:08:25 +0530

    Update args.py

--- Model/args.py
+++ Model/args.py
@@ -16,3 +16,15 @@ class config:
     weight_decay_2 = 0.0
     epochs = 100
     best_val_min = -1
+
+# lr = 3e-2   			         #Learning rate
+# k_splits = 6 			         #K-fold validation with fold=6
+# max_len = 256 		         #Maximum length of protein sequence under consideration
+# expected_n_channels = 57   	 #Number of channels
+# protein_len_consider = 50 	 #Minimum length of protein sequence
+# weight_decay_1 = 0.001   	     #Weight decay from model layers
+# weight_decay_2 = 0.0		     #Weight decay from model layers
+# epochs = 100			         #Number of epochs
+# best_val_min = -1		         #Initial validation score
+# pad_size = 10                  #For convolutions
+
